d62941303b2b7b0036042355f133bebb8ea9bad3
Kurstan <kurstanerkinbaev2@gmail.com>
2023-07-10 00:38:46 +0300

    first kurstan's commit

---
+++ src/main/java/com/example/baygo/BaygoApplication.java
@@ -0,0 +1,13 @@
+package com.example.baygo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaygoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaygoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/baygo/BaygoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baygo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaygoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
