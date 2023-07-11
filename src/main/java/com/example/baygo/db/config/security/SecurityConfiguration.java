b1add717959d753c6a4035b00cda95ef95c98193
devolden <altynbek.shakirov695@gmail.com>
2023-07-11 11:38:08 +0300

    commit

---
+++ src/main/java/com/example/baygo/db/config/security/SecurityConfiguration.java
@@ -0,0 +1,19 @@
+package com.example.baygo.db.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+//
+//    }
+}
---
+++ src/main/java/com/example/baygo/db/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.baygo.db.repository;
+
+import com.example.baygo.db.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
