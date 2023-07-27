a6e6cbdc8e708155548d14334b143aa42f673961
ZHIIDEGUL <emranlini@gmail.com>
2023-07-27 12:19:33 +0600

    get all supplies

--- src/main/java/com/example/baygo/db/api/SellerLandingApi.java
+++ src/main/java/com/example/baygo/db/api/SellerLandingApi.java
@@ -4,8 +4,10 @@ import com.example.baygo.db.dto.response.OrderWareHouseResponse;
 import com.example.baygo.db.dto.response.SupplyLandingPage;
 import com.example.baygo.db.service.OrderService;
 import com.example.baygo.db.service.SupplyService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,16 +20,19 @@ import java.util.List;
 @RequiredArgsConstructor
 @Tag(name = "Landing Seller API")
 @CrossOrigin(origins = "*", maxAge = 3600)
+@PreAuthorize("hasAuthority('SELLER')")
 public class SellerLandingApi {
     private final OrderService orderService;
     private final SupplyService supplyService;
+    @Operation(summary = "The method will get all orders percentage divide into ware house location")
     @GetMapping("/orders_percentage")
-    public List<OrderWareHouseResponse> getAllOrdersByWareHouse(){
+    public List<OrderWareHouseResponse> getAllOrdersByWareHouse() {
         return orderService.getAllOrdersByWareHouse();
 
     }
+    @Operation(summary = "The method will get  3 supplies")
     @GetMapping("/supplies")
-    public List<SupplyLandingPage>getAllSupplyForLanding(){
+    public List<SupplyLandingPage> getAllSupplyForLanding() {
         return supplyService.getAllSupplyForLanding();
     }
 
