0ad98b1dba3d35b6c2273fe1c8d971e9ff920d6b
Kurstan <kurstanerkinbaev2@gmail.com>
2023-07-29 01:36:45 +0300

    Conflicts resolved

--- src/main/java/com/example/baygo/service/impl/ReviewServiceImpl.java
+++ src/main/java/com/example/baygo/service/impl/ReviewServiceImpl.java
@@ -1,15 +1,11 @@
 package com.example.baygo.service.impl;
 
 import com.example.baygo.config.jwt.JwtService;
-import com.example.baygo.db.config.jwt.JwtService;
 import com.example.baygo.db.dto.response.GetAllReviewsResponse;
 import com.example.baygo.db.dto.response.PaginationReviewResponse;
 import com.example.baygo.db.dto.response.ReviewResponse;
 import com.example.baygo.repository.custom.CustomReviewRepository;
 import com.example.baygo.service.ReviewService;
-import com.example.baygo.db.repository.custom.CustomGetAllReviewsRepository;
-import com.example.baygo.db.repository.custom.CustomReviewRepository;
-import com.example.baygo.db.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
