e4d605fea17aa0115faf50b32431f1eaadb92fd7
ZHIIDEGUL <emranlini@gmail.com>
2023-08-03 14:28:36 +0600

    get wwekly analisys

--- src/main/java/com/example/baygo/repository/OrderRepository.java
+++ src/main/java/com/example/baygo/repository/OrderRepository.java
@@ -1,5 +1,6 @@
 package com.example.baygo.repository;
 
+import com.example.baygo.db.dto.response.AnalysisResponse;
 import com.example.baygo.db.dto.response.OrderResponse;
 import com.example.baygo.db.dto.response.RecentOrdersResponse;
 import com.example.baygo.db.model.Order;
@@ -12,6 +13,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -37,6 +39,30 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
             "AND (:status IS NULL OR o.status = :status) " +
             "ORDER BY o.dateOfOrder DESC")
     Page<OrderResponse> getAllOrders(Long sellerId, String keyword, Status status, Pageable pageable);
-}
+
+    @Query("SELECT NEW com.example.baygo.db.dto.response.AnalysisResponse(" +
+            "SUM(CASE WHEN o.dateOfOrder BETWEEN :startDate AND :endDate THEN o.totalPrice ELSE 0 END), " +
+            "SUM(CASE WHEN o.dateOfOrder BETWEEN :startDate AND :endDate THEN o.commission ELSE 0 END)) " +
+            "FROM Order o " +
+            "JOIN Supply s ON o.status = s.status " +
+            "WHERE o.warehouse.id = :warehouseId " +
+            "AND o.seller.id = :sellerId " +
+            "AND o.commission <> 0 " +
+            "AND (:nameofTime IS NULL OR " +
+            "(LOWER(:nameofTime) = 'day' AND DATE_TRUNC('day', o.dateOfOrder) = CAST(:startDate AS DATE)) OR " +
+            "(LOWER(:nameofTime) = 'week' AND o.dateOfOrder >= DATE_TRUNC('week', CAST(:startDate AS DATE)) " +
+            "AND o.dateOfOrder <= DATE_TRUNC('week', CAST(:endDate AS DATE)) + interval '6 days') OR " +
+            "(LOWER(:nameofTime) = 'month' AND o.dateOfOrder >= DATE_TRUNC('month', CAST(:startDate AS DATE)) " +
+            "AND o.dateOfOrder <= DATE_TRUNC('month', CAST(:endDate AS DATE)) + interval '1 month - 1 day'))")
+        AnalysisResponse getWeeklyAnalysis(
+                @Param("startDate") Date startDate,
+                @Param("endDate") Date endDate,
+                @Param("warehouseId") Long warehouseId,
+                @Param("nameofTime") String nameofTime,
+                @Param("sellerId") Long sellerId
+        );
+    }
+
+
 
 
