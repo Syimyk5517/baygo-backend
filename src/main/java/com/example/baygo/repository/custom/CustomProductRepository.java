bceae890a7fd51af08bb699f25fb24ae21e3d1d5
Nuriza05 <mnuriza9@gmail.com>
2023-08-05 17:04:33 +0300

    refactor

--- src/main/java/com/example/baygo/repository/custom/CustomProductRepository.java
+++ src/main/java/com/example/baygo/repository/custom/CustomProductRepository.java
@@ -1,16 +1,11 @@
 package com.example.baygo.repository.custom;
 
 import com.example.baygo.db.dto.response.PaginationResponse;
-import com.example.baygo.db.dto.response.ProductBuyerResponse;
 import com.example.baygo.db.dto.response.ProductResponseForSeller;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigDecimal;
-import java.util.List;
 
 @Repository
 public interface CustomProductRepository {
     PaginationResponse<ProductResponseForSeller> getAll(Long sellerId, String status, String keyWord, int page, int size);
-
-    PaginationResponse<ProductBuyerResponse> getAllProductsBuyer(String keyWord, List<String> sizes, List<String> compositions, List<String> brands, BigDecimal minPrice, BigDecimal maxPrice, List<String> colors, String sortBy, int page, int pageSize);
 }
