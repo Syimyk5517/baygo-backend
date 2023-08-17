c9a2bf2d81ee4b5bf2b8efe9d3050f59f2967f37
Syimykjumabekov <syjmykzumabekuulu@gmail.com>
2023-08-17 18:51:19 +0600

    refactor find all supplies of seller

--- src/main/java/com/example/baygo/api/seller/SellerSupplyController.java
+++ src/main/java/com/example/baygo/api/seller/SellerSupplyController.java
@@ -31,9 +31,10 @@ public class SellerSupplyController {
     PaginationResponse<SuppliesResponse> getAllSuppliesOfSeller
             (@RequestParam(required = false) String supplyNumber,
              @RequestParam(required = false) SupplyStatus status,
+             @RequestParam(defaultValue = "false") Boolean isCreatedAt,
              @RequestParam(defaultValue = "1") int page,
              @RequestParam(defaultValue = "15") int pageSize) {
-        return service.getAllSuppliesOfSeller(supplyNumber, status, page, pageSize);
+        return service.getAllSuppliesOfSeller(supplyNumber, status,isCreatedAt, page, pageSize);
     }
 
     @Operation(summary = "Get all supply products", description = "This method to get all and search supply products")
--- src/main/java/com/example/baygo/repository/custom/impl/SupplyCustomRepositoryImpl.java
+++ src/main/java/com/example/baygo/repository/custom/impl/SupplyCustomRepositoryImpl.java
@@ -1,16 +1,15 @@
 package com.example.baygo.repository.custom.impl;
 
 import com.example.baygo.db.dto.response.PaginationResponse;
-import com.example.baygo.db.dto.response.SuppliesResponse;
 import com.example.baygo.db.dto.response.SupplyLandingPage;
 import com.example.baygo.db.dto.response.SupplyProductResponse;
+import com.example.baygo.db.dto.response.SupplyTransitDirectionResponse;
 import com.example.baygo.db.dto.response.deliveryFactor.DeliveryFactorResponse;
 import com.example.baygo.db.dto.response.deliveryFactor.SupplyTypeResponse;
 import com.example.baygo.db.dto.response.deliveryFactor.WarehouseCostResponse;
 import com.example.baygo.db.model.enums.SupplyStatus;
 import com.example.baygo.db.model.enums.SupplyType;
 import com.example.baygo.repository.custom.SupplyCustomRepository;
-import com.example.baygo.db.dto.response.SupplyTransitDirectionResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
@@ -26,54 +25,6 @@ import java.util.List;
 public class SupplyCustomRepositoryImpl implements SupplyCustomRepository {
     private final JdbcTemplate jdbcTemplate;
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-
-    @Override
-    public PaginationResponse<SuppliesResponse> getAllSuppliesOfSeller(Long currentUserId, String supplyNumber, SupplyStatus status, int page, int pageSize) {
-
-        String sql = """
-                SELECT s.id , s.supply_number, s.supply_type, s.created_at, s.quantity_of_products, s.accepted_products,
-                s.commission, s.supply_cost, s.planned_date, s.actual_date, u.phone_number, s.status
-                FROM supplies s
-                JOIN sellers s2 on s.seller_id = s2.id
-                JOIN users u on s2.user_id = u.id
-                WHERE u.id =""" + currentUserId;
-
-        if (supplyNumber != null && !supplyNumber.isEmpty()) {
-            sql += " AND s.supply_number iLIKE '" + supplyNumber + "%'";
-        }
-
-        if (status != null && status.describeConstable().isPresent()) {
-            sql += " AND s.status = '" + status + "'";
-        }
-
-        sql += " ORDER BY s.created_at DESC ";
-
-        int totalCount = totalCount(sql, pageSize);
-
-        int offset = (page - 1) * pageSize;
-        sql += " LIMIT " + pageSize + " OFFSET " + offset;
-
-        List<SuppliesResponse> suppliesResponses = jdbcTemplate.query(sql, (rs, rowNum) ->
-                SuppliesResponse.builder()
-                        .id(rs.getLong("id"))
-                        .supplyNumber(rs.getString("supply_number"))
-                        .supplyType(rs.getString("supply_type"))
-                        .createdAt(rs.getDate("created_at").toLocalDate())
-                        .quantityOfProducts(rs.getInt("quantity_of_products"))
-                        .acceptedProducts(rs.getInt("accepted_products"))
-                        .commission(rs.getInt("commission"))
-                        .supplyCost(rs.getBigDecimal("supply_cost"))
-                        .plannedDate(rs.getDate("planned_date").toLocalDate())
-                        .actualDate(rs.getDate("actual_date").toLocalDate())
-                        .user(rs.getString("phone_number"))
-                        .status(SupplyStatus.valueOf(rs.getString("status")))
-                        .build());
-        return PaginationResponse.<SuppliesResponse>builder()
-                .elements(suppliesResponses)
-                .currentPage(page)
-                .totalPages(totalCount)
-                .build();
-    }
 
     @Override
     public PaginationResponse<SupplyProductResponse> getSupplyProducts(Long sellerId, Long supplyId, String keyWord, int page, int size) {
