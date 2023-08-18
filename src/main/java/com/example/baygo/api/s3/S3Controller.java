bdb27b723baab0b35de426c94c24397022197164
Kurstan <kurstanerkinbaev2@gmail.com>
2023-08-18 21:41:29 +0300

    Added produces to s3 upload endpoint

--- src/main/java/com/example/baygo/api/s3/S3Controller.java
+++ src/main/java/com/example/baygo/api/s3/S3Controller.java
@@ -4,6 +4,7 @@ import com.example.baygo.service.S3Service;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,7 +23,8 @@ public class S3Controller {
     private final S3Service s3Service;
 
     @Operation(summary = "Upload a file to S3 bucket", description = "Uploads a file to the specified S3 bucket.")
-    @PostMapping(consumes = "multipart/form-data")
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+                 produces = MediaType.APPLICATION_JSON_VALUE )
     public ResponseEntity<Map<String, String>> uploadFiles(@RequestParam("files") MultipartFile files) throws IOException {
         return ResponseEntity.ok(s3Service.uploadFile(files));
     }
