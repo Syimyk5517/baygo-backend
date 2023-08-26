c2ba5355e73c504151ac280f024aecf60a906ca1
Nuriza05 <mnuriza9@gmail.com>
2023-08-26 10:49:01 +0300

    Cancellation of discount!

--- src/main/java/com/example/baygo/repository/SubProductRepository.java
+++ src/main/java/com/example/baygo/repository/SubProductRepository.java
@@ -4,6 +4,7 @@ import com.example.baygo.db.model.SubProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface SubProductRepository extends JpaRepository<SubProduct, Long> {
 }
