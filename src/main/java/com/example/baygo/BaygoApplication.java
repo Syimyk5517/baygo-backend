c215d86786ddb038f57e3b14a8c8b36b636d9837
Kurstan <kurstanerkinbaev2@gmail.com>
2023-08-29 14:59:12 +0300

    Added greetings main

--- src/main/java/com/example/baygo/BaygoApplication.java
+++ src/main/java/com/example/baygo/BaygoApplication.java
@@ -3,12 +3,19 @@ package com.example.baygo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @SpringBootApplication
 @EnableScheduling
 public class BaygoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BaygoApplication.class, args);
+    }
+
+    @RequestMapping
+    public void greeting(){
+        System.out.println("Welcome to Buygo");
     }
 
 }
