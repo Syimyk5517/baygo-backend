b163c12e4d2c98761c7c08c3b0b9d2e85481a466
Nuriza05 <mnuriza9@gmail.com>
2023-09-08 16:03:59 +0300

    Refactored authorize of buyerCategoryController!

--- src/main/java/com/example/baygo/api/buyer/BuyerCategoryController.java
+++ src/main/java/com/example/baygo/api/buyer/BuyerCategoryController.java
@@ -1,13 +1,11 @@
 package com.example.baygo.api.buyer;
 
 import com.example.baygo.db.dto.response.BuyerCategoryResponse;
-import com.example.baygo.db.dto.response.PaginationResponse;
-import com.example.baygo.db.dto.response.buyer.FavoriteResponse;
 import com.example.baygo.service.CategoryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.security.PermitAll;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,12 +15,12 @@ import java.util.List;
 @RequiredArgsConstructor
 @Tag(name = "Category Buyer API")
 @CrossOrigin(origins = "*", maxAge = 3600)
-@PreAuthorize("hasAuthority('BUYER')")
 public class BuyerCategoryController {
     private final CategoryService categoryService;
 
     @GetMapping
     @Operation(summary = "Get categories", description = "This method gets all categories with subcategories!")
+    @PermitAll
     public List<BuyerCategoryResponse> getAllCategories() {
         return categoryService.getAllCategoriesWithSubCategories();
     }
