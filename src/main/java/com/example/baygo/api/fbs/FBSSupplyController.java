97b66ca10ff2541749b96e228b8dbf1f00542da1
Kurstan <kurstanerkinbaev2@gmail.com>
2023-09-13 15:14:58 +0300

    Added api to url fbs apis

--- src/main/java/com/example/baygo/api/fbs/FBSSupplyController.java
+++ src/main/java/com/example/baygo/api/fbs/FBSSupplyController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/fbs/supply")
+@RequestMapping("/api/fbs/supply")
 @RequiredArgsConstructor
 @Tag(name = "Fbs Supply ")
 @CrossOrigin(value = "*", maxAge = 3600)
