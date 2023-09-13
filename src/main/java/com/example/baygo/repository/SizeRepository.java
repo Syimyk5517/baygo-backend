9c09e2adf5f426ff6d5a1d08c52e9b8dc1486b29
Syimykjumabekov <syjmykzumabekuulu@gmail.com>
2023-09-13 19:06:53 +0600

    refactor delete and get all buyer favorites

--- src/main/java/com/example/baygo/repository/SizeRepository.java
+++ src/main/java/com/example/baygo/repository/SizeRepository.java
@@ -14,11 +14,11 @@ import java.util.List;
 
 @Repository
 public interface SizeRepository extends JpaRepository<Size, Long> {
-    @Query("SELECT NEW com.example.baygo.db.dto.response.buyer.FavoriteResponse(p.id, sp.mainImage, p.name, p.rating, r.amountOfLike, sp.price) " +
-            "FROM Product p " +
-            "JOIN p.subProducts sp " +
-            "JOIN sp.reviews r " +
-            "WHERE r.buyer.id = :buyerId " +
+    @Query("SELECT NEW com.example.baygo.db.dto.response.buyer.FavoriteResponse(p.id, bf.id, bf.mainImage, p.name, p.rating, bf.price) " +
+            "FROM Buyer b " +
+            "JOIN b.favorites bf  " +
+            "JOIN bf.product p " +
+            "WHERE b.id = :buyerId " +
             "AND (:search IS NULL OR p.name ILIKE %:search% ) " )
     Page<FavoriteResponse> getBuyerFavorites(
             @Param("buyerId") Long buyerId,
